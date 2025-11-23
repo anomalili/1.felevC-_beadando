0ac78faded925b08c7db68ba0c94bf18ba7fc765
mzsl <95681537+anomalili@users.noreply.github.com>
2025-11-23 16:10:11 +0100

    Add files via upload

---
+++ mondatMegforditasa.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ez a program megfordítja a megadott mondatot!");
+
+        Console.WriteLine("Adj meg egy mondatot!");
+        string mondat = Console.ReadLine();
+
+        char[] karakterek = mondat.ToCharArray();
+        Array.Reverse(karakterek);
+
+        string forditott = new string(karakterek);
+
+        Console.WriteLine("Megfordítva: " + forditott);
+
+    }
+}
