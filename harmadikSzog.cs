ce56e0689e3a28a8b5edee0fb0f9df07b0101ae6
mzsl <95681537+anomalili@users.noreply.github.com>
2025-11-23 10:27:47 +0100

    Add files via upload
    elso feladat feltoltese

---
+++ harmadikSzog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cSzog;
+            
+            Console.WriteLine("Ez a program kiszámolja a képzeletbeli háromszögünk harmadik szögét, miután megadtuk az első két szögét!");
+            Console.WriteLine("Adja meg az 'A' szög értékét!");
+            int aSzog = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg az 'B' szög értékét!");
+            int bSzog = Convert.ToInt32(Console.ReadLine());
+
+            cSzog = 180 - (bSzog + aSzog);
+
+            Console.WriteLine("A háromszög harmadik szögének értéke " + cSzog);
+
+        }
+    }
+}
